686d98d6a8dd17843382bbf3deb21579baf8ac08
Harsh Bhatt <162444490+Harsh-BH@users.noreply.github.com>
2025-06-15 18:06:44 +0530

    0064-MINIMUM-PATH-SUM Stats: Time: 0 ms (100%), Space: 16.3 MB (39.02%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int rec(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        int m = grid.size();
-        int n = grid[0].size();
-        if (i >= m || j >= n) return 1e9;  // out of bounds
-        if (i == m - 1 && j == n - 1) return grid[i][j];  // base case: destination cell
 
-        if (dp[i][j] != -1) return dp[i][j];
+    int rec(int i , int j ,vector<vector<int>>& grid ,vector<vector<int>>& dp){
+        int n = grid.size();
+        int m = grid[0].size();
 
-        int down = rec(i + 1, j, grid, dp);
-        int right = rec(i, j + 1, grid, dp);
+        if(i>=n || j>=m) return 1e9;
 
-        return dp[i][j] = grid[i][j] + min(down, right);
+        if(i==n-1 and j==m-1){
+            return grid[i][j];
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int right = rec(i , j+1 , grid ,dp);
+        int down = rec(i+1 , j , grid ,dp); 
+
+        return dp[i][j] = grid[i][j] + min(right, down);
+
+
     }
-
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return rec(0, 0, grid, dp);
+        vector<vector<int>> dp;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        dp.assign(n+1,vector<int>(m+1,-1));
+
+        return rec(0,0, grid ,dp);
     }
 };
